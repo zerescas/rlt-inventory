569ebf19ae47825c360d4fa58a048cf40552f5db
zerescas <zerescas@gmail.com>
2024-08-06 16:18:44 +0900

    App.vue: define layout for future components, add InventorySidePanelSkeleton

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InventorySidePanelSkeleton from './components/InventorySidePanelSkeleton.vue'
+</script>
 
 <template>
-  <div class="app-layout">App</div>
+  <div class="app-layout">
+    <div class="inventory">
+      <InventorySidePanelSkeleton style="grid-area: inventory-sidepanel" />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .app-layout {
   margin: auto;
+}
+
+.inventory {
+  display: grid;
+  gap: var(--theme-gap-layout);
+  grid-template: 500px 72px / 236px 525px;
+  grid-template-areas:
+    'inventory-sidepanel inventory-grid'
+    'inventory-footer    inventory-footer';
+  height: 100%;
+  width: 100%;
 }
 </style>
