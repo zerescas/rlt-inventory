4d9b22948d4372e9f71a06aaa13a3fbb64bef538
zerescas <zerescas@gmail.com>
2024-08-08 23:56:43 +0900

    skeleton.png: move from "assets" to "public", fix path in InventorySidePanelSkeleton.vue

--- src/components/InventorySidePanelSkeleton.vue
+++ src/components/InventorySidePanelSkeleton.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import AnimatedPlaceholder from './AnimatedPlaceholder.vue';
-import skeletonHeaderImage from '@/assets/img/sidepanel/skeleton.png';
 </script>
 
 <template>
   <div class="inventory-sidepanel-card skeleton">
     <div class="header">
-      <img :src="skeletonHeaderImage" alt="skeleton-img" />
+      <img src="/img/sidepanel/skeleton.png" alt="skeleton-img" />
       <AnimatedPlaceholder width="190px" height="26px" borderRadius="8px" />
     </div>
 
