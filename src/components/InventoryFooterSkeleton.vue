5be4245681a8988c6dd7bdee553697d2e69e6539
zerescas <zerescas@gmail.com>
2024-08-09 14:28:48 +0900

    InventoryFooterSkeleton.vue: add close button

--- src/components/InventoryFooterSkeleton.vue
+++ src/components/InventoryFooterSkeleton.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import AnimatedPlaceholder from './AnimatedPlaceholder.vue';
+import IconClose from './icons/IconClose.vue';
 </script>
 
 <template>
   <div class="inventory-footer-card skeleton">
-    <AnimatedPlaceholder width="100%" height="36px" borderRadius="12px" />
+    <div class="content">
+      <AnimatedPlaceholder width="100%" height="36px" borderRadius="12px" />
+    </div>
+
+    <button class="icon-button card-close-button">
+      <IconClose />
+    </button>
   </div>
 </template>
