a1a6c21c786e9d4eef6fbeb67f12fe325dc576af
zerescas <zerescas@gmail.com>
2024-08-09 14:27:06 +0900

    create IconClose.vue

---
+++ src/components/icons/IconClose.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <svg width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M12 1.05 10.95 0 6 4.95 1.05 0 0 1.05 4.95 6 0 10.95 1.05 12 6 7.05 10.95 12 12 10.95 7.05 6 12 1.05Z"
+      fill="currentColor"
+    />
+  </svg>
+</template>
